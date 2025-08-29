a1e3a2d5eed340e5782c42acf8bc15dfeb03eda2
Paul Dino Jones <paul@spacefreak18.xyz>
2025-08-29 01:49:58 -0400

    Add simdash example

---
+++ examples/web/simdash1/simscript.js
@@ -0,0 +1,43 @@
+
+function updateFlag(flagstatus) {
+  switch(flagstatus) {
+    case 1:
+      document.body.style.backgroundImage=""
+      document.body.style.backgroundColor = "#0000ff";
+      break;
+    case 2:
+      document.body.style.backgroundImage=""
+      document.body.style.backgroundColor = "#ffff00";
+      break;
+    case 3:
+      document.body.style.backgroundImage=""
+      document.body.style.backgroundColor = "#000000";
+      break;
+    case 4:
+      document.body.style.backgroundImage=""
+      document.body.style.backgroundColor = "#ffffff";
+      break;
+    case 5:
+      document.body.style.backgroundImage="url(https://wallpapercave.com/wp/wp4980392.png)"
+      break;
+    case 6:
+      document.body.style.backgroundImage="url(https://xmple.com/wallpaper/squares-orange-checkered-black-1920x1080-c2-ff8c00-000000-l-120-a-85-f-2.svg)"
+      break;
+    case 8:
+      document.body.style.backgroundImage=""
+      document.body.style.backgroundColor = "#ffa500";
+      break;
+    default:
+      document.body.style.backgroundImage=""
+      document.body.style.backgroundColor = "#3cd070";
+  }
+}
+
+
+function simScript() {
+
+  flag = parseInt(document.getElementById("flag").innerHTML);
+  updateFlag(flag);
+}
+
+

3963ca7fb9607951d8ce880acc904dbb49b31628
Paul Dino Jones <paul@spacefreak18.xyz>
2026-05-27 23:09:55 -0400

    move to libarchive over libtar

--- src/simmonitor/helper/iohelper.c
+++ src/simmonitor/helper/iohelper.c
@@ -1,40 +1,75 @@
+#include <archive.h>
+#include <archive_entry.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <tar.h>
-#include <sys/stat.h>
 #include <unistd.h>
+#include <sys/stat.h>
+
+int copy_data(struct archive *ar, struct archive *aw) {
+    int r;
+    const void *buff;
+    size_t size;
+    la_int64_t offset;
+
+    for (;;) {
+        r = archive_read_data_block(ar, &buff, &size, &offset);
+        if (r == ARCHIVE_EOF) return (ARCHIVE_OK);
+        if (r < ARCHIVE_OK) return (r);
+        r = archive_write_data_block(aw, buff, size, offset);
+        if (r < ARCHIVE_OK) {
+            fprintf(stderr, "Write error: %s\n", archive_error_string(aw));
+            return (r);
+        }
+    }
+}
 
 void extract_tarball(const char *tarball_path, const char *extract_dir) {
-    TAR *tar;
-    int result;
+    struct archive *a;
+    struct archive *ext;
+    struct archive_entry *entry;
+    int flags;
+    int r;
 
-    // Open the tarball
-    result = tar_open(&tar, tarball_path, NULL, 0, 0, TAR_GNU);
-    if (result != 0) {
-        perror("tar_open");
-        exit(EXIT_FAILURE);
-    }
+    /* Preservation flags */
+    flags = ARCHIVE_EXTRACT_TIME | ARCHIVE_EXTRACT_PERM | ARCHIVE_EXTRACT_ACL | ARCHIVE_EXTRACT_FFLAGS;
 
-    // Change directory to the extraction directory
+    a = archive_read_new();
+    archive_read_support_format_all(a);
+    archive_read_support_filter_all(a);
+
+    ext = archive_write_disk_new();
+    archive_write_disk_set_options(ext, flags);
+    archive_write_disk_set_standard_lookup(ext);
+
+    /* Target directory check */
     if (chdir(extract_dir) != 0) {
         perror("chdir");
         exit(EXIT_FAILURE);
     }
 
-    // Extract the tarball
-    result = tar_extract_all(tar, NULL);
-    if (result != 0) {
-        perror("tar_extract_all");
+    if ((r = archive_read_open_filename(a, tarball_path, 10240))) {
+        fprintf(stderr, "Could not open %s: %s\n", tarball_path, archive_error_string(a));
         exit(EXIT_FAILURE);
     }
 
-    // Close the tarball
-    result = tar_close(tar);
-    if (result != 0) {
-        perror("tar_close");
-        exit(EXIT_FAILURE);
+    /* Main Extraction Loop */
+    while (archive_read_next_header(a, &entry) == ARCHIVE_OK) {
+        r = archive_write_header(ext, entry);
+        if (r < ARCHIVE_OK) {
+            fprintf(stderr, "Header error: %s\n", archive_error_string(ext));
+        } else if (archive_entry_size(entry) > 0) {
+            r = copy_data(a, ext);
+            if (r < ARCHIVE_OK) {
+                fprintf(stderr, "Data error: %s\n", archive_error_string(a));
+            }
+        }
+        archive_write_finish_entry(ext);
     }
+
+    archive_read_close(a);
+    archive_read_free(a);
+    archive_write_close(ext);
+    archive_write_free(ext);
 
     printf("Extraction complete.\n");
 }
